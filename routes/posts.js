a1d46691e512926d84b86d25d9050c1369931787
Rachel-Fan <fanrg@mail.uc.edu>
2023-11-04 23:40:04 -0700

    .

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // @route   GET api/posts
 // @desc    TEST ROUTE
 // @access  Public
-router.get('/',(req,res) => res.send('Posts route');)
+router.get('/',(req,res) => res.send('Posts route'));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // @route   GET api/users
 // @desc    TEST ROUTE
 // @access  Public
-router.get('/',(req,res) => res.send('User route');)
+router.get('/',(req,res) => res.send('User route'));
 
 module.exports = router;
