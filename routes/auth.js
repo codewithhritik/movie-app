f0541a4a4e8cd22c3ada0f08779cfee9b6ac8507
Rachel-Fan <fanrg@mail.uc.edu>
2023-11-04 23:56:32 -0700

    .

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // @route   GET api/auth
 // @desc    TEST ROUTE
 // @access  Public
-router.get('/',(req,res) => res.send('Auth route'));
+router.get('/',(req, res) => res.send('Auth route'));
 
 module.exports = router;
