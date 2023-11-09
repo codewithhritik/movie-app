53c205025b71a0dd2463bc8515d7eb3432f9654d
Parth joshi <parthjoshi@Parths-MacBook-Air.local>
2023-11-09 13:01:54 -0800

    PJ-Landing Page UI completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,41 @@ import './App.css';
 import LoginSignupPage from './LoginPage'; // Import the LoginSignupPage component
 import logo from './logo.svg'; // Import the logo image
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LandingPage from './pages/LandingPage/LandingPage.js';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginSignupPage />
+  }
+  
+]);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> {/* Display the logo */}
-        <LoginSignupPage /> {/* Include the LoginSignupPage component here */}
-      </header>
+    <div className="main">
+      <RouterProvider router={router} />
     </div>
   );
+
 }
 
 export default App;
+
+
+// return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" /> {/* Display the logo */}
+  //       <LoginSignupPage /> {/* Include the LoginSignupPage component here */}
+  //     </header>
+  //   </div>
+  // );
---
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,94 @@
+import React,{useState, useEffect, useRef} from 'react';
+import './LandingPage.css'
+import Navbar from '../../components/Navbar/Navbar';
+import getMovies from '../../utility/getMovies';
+import PosterCard from '../../components/PosterCard/PosterCard';
+import Button from '../../components/Button/Button';
+import MembershipBox from '../../components/MembershipBox/MembershipBox';
+import BookingPage from '../BookingPage/BookingPage';
+
+
+const LandingPage = () => {
+  
+  
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState([])
+  
+  const bookingPageRef = useRef();
+
+
+
+
+  const handlePosterCardClick = (movie) => {
+
+    setSelectedMovie(movie);
+
+    // Scroll to the bookingPage section with an offset
+    if (bookingPageRef.current) {
+      const offset = -600; // Adjust the offset as needed
+      const targetPosition = bookingPageRef.current.offsetTop - offset;
+
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth',
+      });
+    }
+    
+  }
+
+  useEffect(() => {
+    // Fetch movies when the component mounts
+    getMovies()
+      .then((data) => {
+        setMovies(data.slice(0,8));
+        setSelectedMovie(data[0]);
+      })
+      .catch((error) => {
+        console.error('An error occurred:', error);
+      });
+  }, []);
+
+  return (
+    <div className='container'>
+      <Navbar />
+      <div className='title-1'>
+        <h1>Now Showing</h1>
+      </div>
+      <div className='movies'>
+        <div className="movie-tiles">
+            {movies.map((movie) => {
+                  return (
+                    <div ref={bookingPageRef} onClick={() => handlePosterCardClick(movie)}>
+                        <PosterCard movie={movie}  />
+                    </div>
+                  ) 
+            })}
+        </div>
+      </div>
+      <div className='proceed-btn'>
+        <Button text={'Proceed'}/>
+      </div>
+      <BookingPage movie={selectedMovie} />
+      <div className='title-2'>
+        <h1>Upcoming Movies</h1>
+      </div>
+    {/* Need to change this part to Upcoming Movies Api */}
+      <div className='movies'>
+        <div className="movie-tiles">
+            {movies.map((movie) => {
+                  return <PosterCard movie={movie} key={movie._id}/>
+            })}
+        </div>
+      </div>
+      <div className='title-2'>
+        <h1>Membership Options</h1>
+      </div>
+      <div className='membership'>
+        <MembershipBox text="Regular"/>
+        <MembershipBox text="Premium"/>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
