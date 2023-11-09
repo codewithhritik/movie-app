d5ab5ffab9f179053320e54c337c31467ad8d0ea
Rachel-Fan <fanrg@mail.uc.edu>
2023-11-08 17:04:20 -0800

    .

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,10 +1,25 @@
 // users.js
 import express from 'express';
 const router = express.Router();
+import { check, validationResult } from 'express-validator';
 
-// @route   GET api/users
-// @desc    TEST ROUTE
+
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/',(req,res) => res.send('User route'));
+router.post('/',[
+    check('name','Name is required').not().isEmpty(),
+    check('email','Please include a valid email').isEmail(),
+    check('password',
+    'Please enter a password with 6 or more characters').isLength({min:6})
+], 
+(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    res.send('User route');
+})
 
 export default router;
