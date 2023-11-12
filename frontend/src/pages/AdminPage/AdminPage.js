17909b342bb13f5f993fd3f0f7fa36e34106a463
Hrithik Bagane <hrithiknanasaheb.bagane@sjsu.edu>
2023-11-12 11:36:38 -0800

    Admin Page UI with Table

--- frontend/src/pages/AdminPage/AdminPage.js
+++ frontend/src/pages/AdminPage/AdminPage.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import getMovies from '../../utility/getMovies'
+import AdminMovieTable from '../../components/AdminMovieTable/AdminMovieTable';
 
 function AdminPage() {
-  return (
-    <div>
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        getMovies()
+        .then((data) => {
+            console.log(data)
+            setMovies(data)
+        })
+    }, [])
+
+    return (
+        <div>
         <Navbar />
-      <p>Admin Page</p>
-    </div>
-  )
+        <p>Admin Page</p>
+        <AdminMovieTable movies={movies} />
+        </div>
+    )
 }
 
 export default AdminPage
