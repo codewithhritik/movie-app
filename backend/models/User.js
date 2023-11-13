372d9b669f11fe141eb015863e599ac3b464109d
Rachel-Fan <fanrg@mail.uc.edu>
2023-11-13 13:31:47 -0800

    .

--- backend/models/User.js
+++ backend/models/User.js
@@ -16,6 +16,45 @@ const UserSchema = new mongoose.Schema({
     avatar:{
         type:String
     },
+    role: {
+        type: String,
+        enum: ['regular', 'premium', 'employee'],
+        default: 'regular'
+    },
+    membership: {
+        type: String,
+        enum: ['free', 'premium'], // for regular and premium users
+        default: 'free'
+    },
+    membershipExpiryDate: {
+        type: Date, // for premium users
+        default: null
+    },
+    rewardsPoints: {
+        type: Number,
+        default: 0
+    },
+    pastMovies: [{
+        movieId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Movie'
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    bookings: [{
+        bookingId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Booking'
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    // additional fields as needed
     date:{
         type:Date,
         default:Date.now
